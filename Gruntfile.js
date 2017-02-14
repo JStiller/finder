f1c1fdd4df8d53d124e33ba22d7bfa04360eeb79
José Stiller <JStiller@users.noreply.github.com>
2017-02-14 21:49:59 +0000

    MVP

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+'use strict';
+module.exports = function(grunt) {
+    pkg: grunt.file.readJSON('package.json'),
+        grunt.initConfig({
+            less: {
+                development: {
+                    options: {
+                        cleancss: false,
+                        syncImport: true,
+                        sourceMap: true,
+                        strictImports: true,
+                        sourceMapFilename: './css/base.css.map',
+                        sourceMapURL: './base.css.map'
+                    },
+                    files: {
+                        './css/base.css': './less/base.less'
+                    }
+                }
+            },
+            watch: {
+                styles: {
+                    // Which files to watch (all .less files recursively in the less directory)
+                    files: ['less/**/*.less'],
+                    tasks: ['less']
+                },
+                js: {
+                    // Which files to watch (all .less files recursively in the less directory)
+                    files: ['javascript/**/*.js'],
+                    tasks: ['uglify']
+                }
+            },
+            uglify: {
+                options: {
+                    mangle: false,
+                    compress: false,
+                    preserveComments: 'all',
+                    beautify: false,
+                    banner: '/*! <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                },
+                general: {
+                    files: {
+                        './javascript/javascript.js': [
+                            'javascript/base.js'
+                        ]
+                    }
+                }
+            }
+        });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['less', 'uglify']);
+};
