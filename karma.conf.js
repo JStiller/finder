5b9f13c3fbed472f14cc7e8d56ef2d1e7acfd55f
José Stiller <JStiller@users.noreply.github.com>
2017-02-15 10:58:19 +0000

    added karma, jasmine and phantomjs

---
+++ karma.conf.js
@@ -0,0 +1,35 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            'node_modules/es6-shim/es6-shim.js',
+            'source/javascript/**/*.js'
+        ],
+        preprocessors: {
+            'source/javascript/*.js': ['coverage']
+        },
+        plugins: [
+            'karma-jasmine',
+            'karma-phantomjs-launcher',
+            'karma-coverage'
+        ],
+        reporters: ['progress', 'coverage'],
+        port: 9878,
+        colors: true,
+        logLevel: config.LOG_DEBUG,
+        autowatch: true,
+        browsers: ['PhantomJS'],
+        singleRun: false,
+        concurrency: Infinity,
+        coverageReporter: {
+            includeAllSources: true,
+            dir: 'coverage/',
+            reporters: [
+                // { type: "html", subdir: "html" },
+                { type: 'text' },
+                { type: 'text-summary' }
+            ]
+        }
+    });
+};
