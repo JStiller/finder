b81307c705f4609ce534a4d58cc2b931a65f96b0
José Stiller <JStiller@users.noreply.github.com>
2017-02-27 18:21:43 +0000

    added multiple search terms

--- source/javascript/base.js
+++ source/javascript/base.js
@@ -3,15 +3,29 @@ var businessLogic = function(finder) {
     var definitionLists = document.querySelectorAll('dl');
     finder.index(definitionLists);
 
-    function search(keyword) {
-        var regEx = new RegExp('(' + keyword + ')', 'i');
+    function search(inputKeyword) {
+        var expression = '.*';
+        inputKeyword.split(' ').forEach(function(keywordPart) {
+            expression = expression.concat('(' + keywordPart + ').*');
+        });
+        var regEx = new RegExp(expression, 'i');
         var show = finder.search(regEx, true);
         var hide = finder.search(regEx, false);
 
         show.forEach(function(nodeList) {
             nodeList[1].forEach(function(node) {
                 dom.show(node['reference']);
-                node['reference'].innerHTML = node['reference'].innerText.replace(regEx, '<mark>$1</mark>');
+                var matches = node['reference'].firstChild.innerText.match(regEx);
+                if(Array.isArray(matches) === true && matches.length > 1) {
+                    var text = node['reference'].firstChild.innerText;
+                    matches.shift();
+                    matches.forEach(function(match) {
+                        if(match.length > 0) {
+                            text = text.replace(match, '<mark>' + match + '</mark>')
+                        }
+                    });
+                    node['reference'].firstChild.innerHTML = text;
+                }
             });
         }, this);
 
